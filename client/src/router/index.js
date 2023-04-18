3b19fa51ce32a25653732d9d5e2afc009e3aa6c4
Charlie <charlielduong@gmail.com>
2023-04-18 15:36:28 -0400

    Moved form table into a component; Removed aboutView Files

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,14 +11,6 @@ const routes = [
     path: '/form',
     name: 'form',
     component: () => import(/* webpackChunkName: "form" */ '../views/FormView.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
