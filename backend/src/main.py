0ca4742953710fda95debc708b7ad2b6676b41f2
Notjcvh <64852956+Notjcvh@users.noreply.github.com>
2023-08-09 01:27:39 -0400

    Tesseract succesfully added to the project

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,16 +3,20 @@ from fastapi import FastAPI, Form, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from src.models import FormItem
 import logging
+import io
+from PIL import Image
+import pytesseract
+import json
+import re
 
 app = FastAPI()
 
-# Allows requests that originate from a different protocol
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["*"], # Allow all ports
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
 )
 
 @app.get("/")
@@ -20,25 +24,71 @@ def home():
     return "This is connected to the backend: main.py"
 
 
-# @app.post("/form")
-# async def submit_form_data(
-#     people: List[str] = Form(...),
-#     numOfPeople: int = Form(...),
-#     amountDue: List[float] = Form(...),
-#     tipAndTax: List[int] = Form(...)
-# ):
-#     new_form_data = FormItem(
-#         people=people,
-#         numOfPeople=numOfPeople,
-#         amountDue=amountDue,
-#         tipAndTax=tipAndTax
-#     )
-#     logging.info("THIS IS RUNNING")
-#     return {"data": new_form_data.dict()}
+@app.post("/form")
+async def submit_form_data(
+    people: List[str] = Form(...),
+    numOfPeople: int = Form(...),
+    amountDue: List[float] = Form(...),
+    tipAndTax: List[int] = Form(...)
+):
+    new_form_data = FormItem(
+        people=people,
+        numOfPeople=numOfPeople,
+        amountDue=amountDue,
+        tipAndTax=tipAndTax
+    )
+    logging.info("THIS IS RUNNING")
+    return {"data": new_form_data.dict()}
+
+def clean_ocr_output(text):
+    # Remove any form feed characters
+    text = text.replace("\f", "")
+    
+    # Split text into lines and clean each line
+    lines = text.split("\n")
+    cleaned_lines = []
+
+    for line in lines:
+        line = line.strip()  # Strip whitespace
+        
+        # Replace multiple spaces with single space
+        line = re.sub(r'\s+', ' ', line)
+        
+        # Only include lines with at least one alphabetic character (filter out lines that are just artifacts)
+        if re.search(r'[A-Za-z]', line):
+            cleaned_lines.append(line)
+
+    # Join the cleaned lines back into a single string
+    cleaned_text = "\n".join(cleaned_lines)
+    
+    return cleaned_text
 
 # Takes an image file and returns its name
 @app.post("/receipt")
 async def upload_receipt(file: UploadFile = File(...)):
-    # Here, you can use file.file to access the uploaded file object
-    # You could use PyTesseract or another library here to process the image
-    return {"filename": file.filename}
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    raw_text = pytesseract.image_to_string(image)
+    cleaned_text = clean_ocr_output(raw_text)
+
+    # Convert newline characters to spaces for a more legible response
+    legible_text = cleaned_text.replace('\n', ' ').strip()
+
+    # Construct the data dictionary
+    data = {
+        "filename": file.filename,
+        "text": legible_text  # Use the legible_text here
+    }
+
+    # Serialize the dictionary to a JSON string
+    json_str = json.dumps(data, indent=4)
+
+    # Define the name for the JSON file
+    json_filename = "output.json"
+
+    # Write the JSON string to a file
+    with open(json_filename, 'w') as json_file:
+        json_file.write(json_str)
+    
+    # Return the legible text
+    return {"text": legible_text}
--- backend/src/models.py
+++ backend/src/models.py
@@ -1,12 +1,12 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-# class FormItem(BaseModel):
-#     people: list[str]
-#     numOfPeople: int
-#     amountDue: list[float]
-#     tip: float
-#     tax: float
+class FormItem(BaseModel):
+    people: list[str]
+    numOfPeople: int
+    amountDue: list[float]
+    tip: float
+    tax: float
 
-# class ReceiptReaderItem(BaseModel):
-#     imagepath: str
+class ReceiptReaderItem(BaseModel):
+    imagepath: str
